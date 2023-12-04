ce87b0f27f97252d29c36a3a33e5ed9b0b855ec9
leanebel <leane.belviso@gmail.com>
2023-12-04 09:54:59 +0100

    Fix Personnage class

--- src/main/java/org/project/modele/Personnage.java
+++ src/main/java/org/project/modele/Personnage.java
@@ -1,13 +1,29 @@
 package org.project.modele;
 
 public class Personnage {
-    private String nom;
-    private int rang;
-    private String caracteristiques;
-    private int nbPermisDeConstruire;
+
+    // Attributs
+    private String nom, caracteristiques;
+    private int rang, nbPermisDeConstruire;
     private Joueur joueur;
-    private boolean estAssassine;
-    private boolean estVole;
+    private boolean estAssassine, estVole;
+
+    /*
+        Constructeur
+        On utilise les setters dès que l'on peut, ici on utilise uniquement le setter de joueur car c'est
+        la seule méthode qui permet de réecrire un attribut en fonction d'un paramètre dans les parentheses
+     */
+    public Personnage(String nom, int rang, String caracteristiques, int nbPermisDeConstruire) {
+        this.nom = nom;
+        this.rang = rang;
+        this.caracteristiques = caracteristiques;
+        this.nbPermisDeConstruire = nbPermisDeConstruire;
+        setJoueur(null);
+        this.estAssassine = false;
+        this.estVole = false;
+    }
+
+    //Getters et setters
 
     public String getNom() {
         return nom;
@@ -21,7 +37,7 @@ public class Personnage {
         return caracteristiques;
     }
 
-    public int getNbPermisDeConstruire() {
+    public int getNbPermis() {
         return nbPermisDeConstruire;
     }
 
@@ -30,106 +46,123 @@ public class Personnage {
         return joueur;
     }
 
-    public boolean isEstAssassine() {
+    public boolean getEstAssassine() {
         return estAssassine;
     }
 
-    public boolean isEstVole() {
+    public boolean getEstVole() {
         return estVole;
     }
 
-    public void setJoueur(Joueur joueur) {
-        this.joueur = joueur;
+    public void setJoueur(Joueur j) {
+        this.joueur = j;
     }
 
-    public void setEstVole(boolean estVole) {
-        this.estVole = estVole;
+    public void setEstVole() {
+        this.estVole = true;
     }
 
-    public void setEstAssassine(boolean estAssassine) {
-        this.estAssassine = estAssassine;
+    public void setEstAssassine() {
+        this.estAssassine = true;
     }
 
-    public Personnage(String nom, int rang, String caracteristiques, int nbPermisDeConstruire) {
-        this.nom = nom;
-        this.rang = rang;
-        this.caracteristiques = caracteristiques;
-        this.nbPermisDeConstruire = nbPermisDeConstruire;
-        this.joueur = null;
+    // Méthode pour ajouter des pièces au trésor du joueur
+    public void ajouterPieces() {
+        // Si le personnage est associé à un joueur et n'est pas assassiné, on ajoute deux pièces au trésor du joueur
+        if (getJoueur() != null && getEstAssassine() == false){
+            getJoueur().ajouterPieces(2);
+            System.out.println("Ajout de deux pièces au trésor du joueur " + getJoueur().getNom());
+        }
+
+        // Si le personnage n'est pas associé à un joueur, on affiche un message d'erreur
+        else if(getJoueur() == null){
+            System.out.println("Aucune pièce ajoutée. Le personnage n'est pas associé à un joueur");
+        }
+
+        // Si le personnage est assassiné, on affiche un message d'erreur
+        else if(getEstAssassine() == true){
+            System.out.println("Aucune pièce ajoutée. Le personnage est assassiné");
+        }
+
+        // Si le personnage n'est pas associé à un joueur et n'est pas assassiné, on affiche un message d'erreur
+        else{
+            System.out.println("Aucune pièce ajoutée. Erreur inconnue");
+        }
+    }
+
+    // Méthode pour piocher un quartier
+    public void ajouterQuartierDansMain(Quartier nouveau) {
+        if (getJoueur() != null && getEstAssassine() == false) {
+            getJoueur().ajouterQuartierDansMain(nouveau);
+            System.out.println("Ajout du quartier " + nouveau.getNom() + " à la main du joueur " + getJoueur().getNom());
+        }
+        else if(getJoueur() == null){
+            System.out.println("Aucun quartier ajouté. Le personnage n'est pas associé à un joueur");
+        }
+        else if(getEstAssassine() == true){
+            System.out.println("Aucun quartier ajouté. Le personnage est assassiné");
+        }
+        else{
+            System.out.println("Aucun quartier ajouté. Erreur inconnue");
+        }
+    }
+
+    // Méthode pour construire un quartier
+    public void construire(Quartier nouveau) {
+        if (getJoueur() != null && getEstAssassine() == false) {
+            getJoueur().ajouterQuartierDansCite(nouveau);
+            System.out.println("Construction du quartier " + nouveau.getNom() + " dans la cité du joueur " + getJoueur().getNom());
+        }
+        else if(getJoueur() == null){
+            System.out.println("Aucun quartier construit. Le personnage n'est pas associé à un joueur");
+        }
+        else if(getEstAssassine() == true){
+            System.out.println("Aucun quartier construit. Le personnage est assassiné");
+        }
+        else{
+            System.out.println("Aucun quartier construit. Erreur inconnue");
+        }
+    }
+
+    // Méthode pour percevoir les ressources spécifiques du personnage
+    public void percevoirRessourcesSpecifiques() {
+        if (getJoueur() != null && getEstAssassine() == false) {
+            System.out.println("Aucune ressource spécifique");
+        }
+        else if(getJoueur() == null){
+            System.out.println("Aucune ressource spécifique. Le personnage n'est pas associé à un joueur");
+        }
+        else if(getEstAssassine() == true){
+            System.out.println("Aucune ressource spécifique. Le personnage est assassiné");
+        }
+        else{
+            System.out.println("Aucune ressource spécifique. Erreur inconnue");
+        }
+    }
+
+    // Méthode pour utiliser le pouvoir du personnage
+    public void utiliserPouvoir() {
+        if (getJoueur() != null && getEstAssassine() == false) {
+            System.out.println("Aucun pouvoir");
+        }
+        else if(getJoueur() == null){
+            System.out.println("Aucun pouvoir. Le personnage n'est pas associé à un joueur");
+        }
+        else if(getEstAssassine() == true){
+            System.out.println("Aucun pouvoir. Le personnage est assassiné");
+        }
+        else{
+            System.out.println("Aucun pouvoir. Erreur inconnue");
+        }
+    }
+
+    // Méthode pour réinitialiser les attributs joueur, estAssassine et estVole
+    public void reinitialiser() {
+        setJoueur(null);
         this.estAssassine = false;
         this.estVole = false;
     }
-        public void ajouterPieces() {
-            if (joueur != null && !estAssassine) {
-                joueur.ajouterPieces(2);
-                System.out.println("Ajout de deux pièces au trésor du joueur " + joueur.getNom());
-            } else {
-                System.out.println("Aucune pièce ajoutée. Le personnage n'est pas associé à un joueur ou est assassiné.");
-            }
-        }
-
-    public void ajouterQuartierDansMain(Quartier nouveau) {
-        if (joueur != null && !estAssassine) {
-            if (joueur.getPossedeCouronne()) {
-                joueur.ajouterQuartierDansMain(nouveau);
-                System.out.println("Ajout du quartier " + nouveau.getNom() + " à la main du joueur " + joueur.getNom());
-            } else {
-                System.out.println("Le joueur " + joueur.getNom() + " ne peut pas ajouter de quartier à sa main. Il ne possède pas de couronne.");
-            }
-        } else {
-            System.out.println("Le personnage n'est pas associé à un joueur ou est assassiné. Aucun quartier ajouté à la main.");
-        }
-    }
-
-        public void construire(Quartier nouveau) {
-
-            if (joueur != null && !estAssassine) {
-
-                if (joueur.getPossedeCouronne()) {
-
-                    joueur.ajouterQuartierDansCite(nouveau);
-                    System.out.println("Construction du quartier " + nouveau.getNom() + " dans la cité du joueur " + joueur.getNom());
-                } else {
-                    System.out.println("Le joueur " + joueur.getNom() + " ne peut pas construire de quartier dans sa cité. Il ne possède pas de couronne.");
-                }
-            } else {
-                System.out.println("Le personnage n'est pas associé à un joueur ou est assassiné. Aucun quartier construit dans la cité.");
-            }
-        }
-        public void percevoirRessourcesSpecifiques() {
-
-            if (joueur != null && !estAssassine) {
-
-                if (joueur.getPossedeCouronne()) {
-
-                    System.out.println("Aucune ressource spécifique");
-                } else {
-                    System.out.println("Le joueur " + joueur.getNom() + " ne peut pas percevoir de ressources spécifiques. Il ne possède pas de couronne.");
-                }
-            } else {
-                System.out.println("Le personnage n'est pas associé à un joueur ou est assassiné. Aucune ressource spécifique perçue.");
-            }
-        }
-
-        // Méthode pour utiliser le pouvoir du personnage
-        public void utiliserPouvoir() {
-            // Vérifier si le personnage est associé à un joueur et n'est pas assassiné
-            if (joueur != null && !estAssassine) {
-                // Traiter le cas où le personnage utilise son pouvoir
-                // (Ajoutez votre logique spécifique ici si nécessaire)
-                System.out.println("Aucun pouvoir");
-            } else {
-                System.out.println("Le personnage n'est pas associé à un joueur ou est assassiné. Aucun pouvoir utilisé.");
-            }
-        }
-
-        // Méthode pour réinitialiser les attributs joueur, estAssassine et estVole
-        public void reinitialiser() {
-            this.joueur = null;
-            this.estAssassine = false;
-            this.estVole = false;
-        }
-    }
+}
 
 
 
