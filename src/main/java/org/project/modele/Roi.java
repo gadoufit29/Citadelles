de5a9cd55aaca7c2ec03492f980c992fe700febe
jeanorliac <jean.orliac1@gmail.com>
2023-12-05 17:04:22 +0100

    Add Roi class

--- src/main/java/org/project/modele/Roi.java
+++ src/main/java/org/project/modele/Roi.java
@@ -1,22 +1,42 @@
 package org.project.modele;
 
 public class Roi extends Personnage {
-    public Roi(String nom, int rang, String caracteristiques, int nbPermisDeConstruire) {
-        super(nom, rang, caracteristiques, nbPermisDeConstruire);
+    private Joueur joueur;
+    private String nom;
+    private int rang;
+    private String caracteristique;
+    private int nbPermisDeConstruire;
+    private boolean estAssasine;
+    private boolean estVole;
+    private PlateauDeJeu plateau;
+
+    public Personnage(String nom, int rang, String caracteristiques, int nbPermisDeConstruire){
+        this.nom = nom;
+        this.rang = rang;
+        this.caracteristique = caracteristiques;
+        this.nbPermisDeConstruire = nbPermisDeConstruire
     }
 
-    public Roi(){
-        super();
-
+    // 2. Implémentez le constructeur vide
+    public Roi() {
+        // Appel du constructeur de la classe mère avec les paramètres spécifiés
+        super("Roi", 4, modele.Caracteristiques.ROI, 1);
     }
 
-    /*public void percevoirRessourcesSpecifiques() {
-        if (plateau != null) {
-            for (Personnage personnage : plateau.getListePersonnages()) {
-                personnage.retirerCouronne();
-            }
+    // 3. Surchargez la méthode percevoirRessourcesSpecifiques()
+    @Override
+    public void percevoirRessourcesSpecifiques() {
+        // Comptabiliser le nombre de quartiers nobles
+        public class QartierNoble {
+
         }
-    }*/
+        getMainJoueur()
+        //int nbQuartiersNobles = joueur.getQuartiersNobles();
 
+        // Ajouter ce nombre au trésor du joueur
+        joueur.ajouterPieces(nbQuartiersNobles);
+
+        // Afficher un message indiquant le nombre de pièces ajoutées
+        System.out.println("Le Roi a perçu " + nbQuartiersNobles + " pièce(s) d'or.");
+    }
 }
-
