f3af572a969eeade83f3d1f479d26364176724bd
gadoufit29 <breche-inne.0b@icloud.com>
2023-12-05 18:35:32 +0100

    Fix Roi class

--- src/main/java/org/project/modele/Roi.java
+++ src/main/java/org/project/modele/Roi.java
@@ -1,42 +1,27 @@
 package org.project.modele;
-
 public class Roi extends Personnage {
-    private Joueur joueur;
-    private String nom;
-    private int rang;
-    private String caracteristique;
-    private int nbPermisDeConstruire;
-    private boolean estAssasine;
-    private boolean estVole;
-    private PlateauDeJeu plateau;
-
-    public Personnage(String nom, int rang, String caracteristiques, int nbPermisDeConstruire){
-        this.nom = nom;
-        this.rang = rang;
-        this.caracteristique = caracteristiques;
-        this.nbPermisDeConstruire = nbPermisDeConstruire
-    }
 
     // 2. Implémentez le constructeur vide
     public Roi() {
         // Appel du constructeur de la classe mère avec les paramètres spécifiés
-        super("Roi", 4, modele.Caracteristiques.ROI, 1);
+        super("Roi", 4, Caracteristiques.ROI, 1);
     }
 
     // 3. Surchargez la méthode percevoirRessourcesSpecifiques()
     @Override
     public void percevoirRessourcesSpecifiques() {
-        // Comptabiliser le nombre de quartiers nobles
-        public class QartierNoble {
-
+        if(super.getJoueur() == null || super.getEstAssassine()) {
+            return;
         }
-        getMainJoueur()
-        //int nbQuartiersNobles = joueur.getQuartiersNobles();
-
-        // Ajouter ce nombre au trésor du joueur
-        joueur.ajouterPieces(nbQuartiersNobles);
-
-        // Afficher un message indiquant le nombre de pièces ajoutées
-        System.out.println("Le Roi a perçu " + nbQuartiersNobles + " pièce(s) d'or.");
+        else{
+            int nbPieces = 0;
+            for(int i = 0; i < this.getJoueur().getCite().size(); i++) {
+                if(this.getJoueur().getCite().get(i).getType().equals("NOBLE")) {
+                    this.getJoueur().ajouterPieces(1);
+                    nbPieces++;
+                }
+            }
+            System.out.println("Le joueur " + this.getJoueur().getNom() + " a gagné " + nbPieces);
+        }
     }
 }
