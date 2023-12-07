3a7d06ad7f9e78b07bd322f769e28e9283038037
gadoufit29 <breche-inne.0b@icloud.com>
2023-12-07 13:00:47 +0100

    Add Eveque and TestEveque class

---
+++ src/main/java/org/project/modele/Eveque.java
@@ -0,0 +1,22 @@
+package org.project.modele;
+
+import java.util.ArrayList;
+
+public class Eveque extends Personnage{
+
+    public Eveque() {
+        super("Eveque", 5, Caracteristiques.EVEQUE, 1);
+    }
+
+    @Override
+    public void percevoirRessourcesSpecifiques() {
+        ArrayList<Quartier> quartierDansCite = this.getJoueur().getCite();
+
+        for(int i = 0 ; i < quartierDansCite.size() ; i++){
+            if(quartierDansCite.get(i).getType().equals("RELIGIEUX")){
+                this.getJoueur().ajouterPieces(1);
+            }
+        }
+
+    }
+}
